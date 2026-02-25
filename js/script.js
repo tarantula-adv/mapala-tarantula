d1b160464d8a374857e26d8614086978aec6c49e
tarantula-adv <tarantulaadventure1@gmail.com>
2026-02-25 10:16:37 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ window.addEventListener("scroll", function() {
     const navbar = document.querySelector(".navbar");
     navbar.classList.toggle("scrolled", window.scrollY > 50);
 });
+<script>
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+</script>
